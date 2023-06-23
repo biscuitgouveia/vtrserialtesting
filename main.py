14e49a82dff2ab8f28c3f255ad12fa1f228b1a7a
Ethan <ethangouveiamain@gmail.com>
2023-06-23 12:55:34 +0800

    Testing

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 import serial
 from time import sleep
+import binascii
 
-com = serial.Serial()
+com = serial.Serial("COM1")
 com.baudrate = 38400
-com.port = "COM1"
+# com.port = "COM1"
 com.parity = serial.PARITY_ODD
 com.stopbits = serial.STOPBITS_ONE
 com.bytesize = serial.EIGHTBITS
+com.timeout = 2
 
 sleep(2)
 
-com.open()
 
 # com.write(bytearray.fromhex((0x02, 0x02, 0x10, 0x01, 0xEF)))
 
-com.write(bytearray.fromhex((0x02, 0x02, 0x30, 0x01, 0xCF)))
+com.write(bytearray.fromhex("0203202101be"))
 
 capture = com.read(20)
 
 com.close()
 
 print(capture)
+new = binascii.b2a_hex(capture)
+print(new)
