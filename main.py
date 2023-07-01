f34c554c09c2c515c7cde37252306ca15126f06b
Ethan Gouveia <ethangouveiamain@gmail.com>
2023-07-01 12:11:55 +0800

    Added main func

--- main.py
+++ main.py
@@ -5,23 +5,29 @@ import binascii
 import commandTypes
 from commandBlocks import CommandBlocks
 
-com = serial.Serial("COM1")
-com.baudrate = 38400
-com.parity = serial.PARITY_ODD
-com.stopbits = serial.STOPBITS_ONE
-com.bytesize = serial.EIGHTBITS
-com.timeout = 2
+
+def main():
+    com = serial.Serial("COM1")
+    com.baudrate = 38400
+    com.parity = serial.PARITY_ODD
+    com.stopbits = serial.STOPBITS_ONE
+    com.bytesize = serial.EIGHTBITS
+    com.timeout = 2
 
 
 
-# com.write(bytearray.fromhex((0x02, 0x02, 0x10, 0x01, 0xEF)))
+    # com.write(bytearray.fromhex((0x02, 0x02, 0x10, 0x01, 0xEF)))
 
-# com.write(bytearray.fromhex("0203202101be"))
+    # com.write(bytearray.fromhex("0203202101be"))
 
-# capture = com.read(20)
+    # capture = com.read(20)
 
-open_port = CommandBlocks(commandTypes.CMD1.SenseRequest.value, commandTypes.SenseRequestCommands.open_port.value)
+    open_port = CommandBlocks(commandTypes.CMD1.SenseRequest.value, commandTypes.SenseRequestCommands.open_port.value)
 
-get_status = CommandBlocks(commandTypes.CMD1.SenseRequest.value, commandTypes.SenseRequestCommands.port_status.value)
+    get_status = CommandBlocks(commandTypes.CMD1.SenseRequest.value, commandTypes.SenseRequestCommands.port_status.value)
 
-com.close()
+    com.close()
+
+
+if __name__ == '__main__':
+    main()
